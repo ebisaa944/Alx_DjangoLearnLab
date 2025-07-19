6ba14becdd069f143744483c636589d951271c6f
Ebisa Achame <ebisaa944@gmail.com>
2025-07-19 11:36:04 +0300

    Added user authentication with login, logout and registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Library
 
 def register_view(request):
     if request.method == 'POST':
