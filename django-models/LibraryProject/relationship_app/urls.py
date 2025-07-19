635abc2a975e844e23c36f2f26a25e4c375b3a6b
Ebisa Achame <ebisaa944@gmail.com>
2025-07-19 11:40:56 +0300

    Added user authentication with login, logout and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,10 @@
+from .views import list_books
+from .views import logout_view
+from .views import login_view
+from .views import register_view
+from .views import LibraryDetailView
 from django.urls import path
-from .views import (list_books, LibraryDetailView, 
-                   register_view, login_view, logout_view)
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
