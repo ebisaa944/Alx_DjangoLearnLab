747f46b53ba39c118b9a28725f9b619cddf6f544
Ebisa Achame <ebisaa944@gmail.com>
2025-07-21 09:20:21 +0300

    all

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,6 +14,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
+from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
 def list_books(request):
