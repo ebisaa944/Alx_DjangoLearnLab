de1f5acba64eb547fd58dc221f29f49b5936ca46
Ebisa Achame <ebisaa944@gmail.com>
2025-07-21 09:06:42 +0300

    all

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,33 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+def get_books_by_author(author_name):
+    """Query all books by a specific author"""
+    return Book.objects.filter(author__name=author_name)
+
+def get_books_in_library(library_name):
+    """List all books in a library"""
+    return Library.objects.get(name=library_name).books.all()
+
+def get_librarian_for_library(library_name):
+    """Retrieve the librarian for a library"""
+    return Librarian.objects.get(library__name=library_name)
+
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
