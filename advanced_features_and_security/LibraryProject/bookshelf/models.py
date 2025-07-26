fcfd7d44002b1e7e351b2347f9b1e79724c23260
Ebisa Achame <ebisaa944@gmail.com>
2025-07-26 09:30:31 +0300

    feat: Add Book model with required custom permissions (can_view, can_create, can_edit, can_delete)

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -37,3 +37,17 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    
+    class Meta:
+        permissions = [
+            ("can_view_book", "Can view book"),
+            ("can_create_book", "Can create book"),
+            ("can_edit_book", "Can edit book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
