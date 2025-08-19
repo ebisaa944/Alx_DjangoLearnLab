a18d174bb739cf570dd32000759e643a8e73a548
Ebisa Achame <ebisaa944@gmail.com>
2025-08-19 11:31:56 +0300

    feat: Add social_media_api project with user authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,11 @@
 # accounts/serializers.py
 
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
+
+# Get the active custom user model
+User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,7 +3,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token # This line is important
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
 class UserRegistrationView(APIView):
@@ -11,7 +11,7 @@ class UserRegistrationView(APIView):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
+            token, created = Token.objects.get_or_create(user=user) # This line is important
             return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -20,6 +20,6 @@ class UserLoginView(APIView):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data
-            token, created = Token.objects.get_or_create(user=user)
+            token, created = Token.objects.get_or_create(user=user) # This line is important
             return Response({'token': token.key})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
