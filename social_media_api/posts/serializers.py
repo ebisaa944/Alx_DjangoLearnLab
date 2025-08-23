cce0c01b4ae9b5a66d095f8d97600f64072fa010
Ebisa Achame <ebisaa944@gmail.com>
2025-08-23 09:50:01 +0300

    Commit after fixing all flaws in a code

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,13 +1,7 @@
-"""
-This module defines the serializers for the Post and Comment models.
-"""
 from rest_framework import serializers
 from .models import Post, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Comment model.
-    """
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
@@ -15,10 +9,8 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
         read_only_fields = ['author']
 
+
 class PostSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Post model.
-    """
     author = serializers.ReadOnlyField(source='author.username')
     comments = serializers.StringRelatedField(many=True, read_only=True)
 
